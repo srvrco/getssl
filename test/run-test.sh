5a3d9af911fb7ec6e814af8ad76977e290ebc62d
Tim Kimber <tkimber@mobius.org.uk>
2020-01-06 07:38:21 +0000

    Add RHEL6 tests, test dns and http

--- test/run-test.sh
+++ test/run-test.sh
@@ -1,9 +1,17 @@
 #! /bin/sh
 
 wget --no-clobber https://raw.githubusercontent.com/letsencrypt/pebble/master/test/certs/pebble.minica.pem
-export CURL_CA_BUNDLE=/root/pebble.minica.pem
+cat /etc/pki/tls/certs/ca-bundle.crt /root/pebble.minica.pem > /root/pebble-ca-bundle.crt
+export CURL_CA_BUNDLE=/root/pebble-ca-bundle.crt
 
+curl -X POST -d '{"host":"getssl", "addresses":["10.30.50.4"]}' http://10.30.50.3:8055/add-a
+
+# Test certificate creation
+cp /getssl/test/test-config/nginx-ubuntu-no-ssl /etc/nginx/sites-enabled/default
 service nginx start
 /getssl/getssl -c getssl
-cp getssl.cfg /root/.getssl/getssl
+cp /getssl/test/test-config/getssl-ubuntu.cfg /root/.getssl/getssl/getssl.cfg
 /getssl/getssl getssl
+
+# Test forced renewal
+/getssl/getssl getssl -f
