6b17701cdce2de30b8408b08d7d121271904943f
Tim Kimber <tkimber@mobius.org.uk>
2020-02-06 17:25:30 +0000

    Use default awk on most images, add debian:latest and ubuntu:latest

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker exec -it getssl-alpine bats /getssl/test
 docker exec -it getssl-centos6 bats /getssl/test
+docker exec -it getssl-debian bats /getssl/test
+docker exec -it getssl-ubuntu bats /getssl/test
 docker exec -it getssl-ubuntu18 bats /getssl/test
-docker exec -it getssl-ubuntu18-no-gawk bats /getssl/test/5-old-awk-error.bats
