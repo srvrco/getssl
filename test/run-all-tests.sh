faef9c43d29e78f2edfbf83f64da4db8bc8e588f
Tim Kimber <tkimber@mobius.org.uk>
2020-02-27 14:04:40 +0000

    Remove interactive

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-docker exec -it getssl-alpine bats /getssl/test
-docker exec -it getssl-centos6 bats /getssl/test
-docker exec -it getssl-debian bats /getssl/test
-docker exec -it getssl-ubuntu bats /getssl/test
-docker exec -it getssl-ubuntu18 bats /getssl/test
-docker exec -it getssl-duckdns bats /getssl/test
+docker exec getssl-alpine bats /getssl/test
+docker exec getssl-centos6 bats /getssl/test
+docker exec getssl-debian bats /getssl/test
+docker exec getssl-ubuntu bats /getssl/test
+docker exec getssl-ubuntu18 bats /getssl/test
+docker exec getssl-duckdns bats /getssl/test
